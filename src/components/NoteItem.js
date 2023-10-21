39b00e04b4b710a27a55c1991ba7cbe02bf2518e
swapnil1303 <swapnilmakwana4@gmail.com>
2023-10-21 11:19:07 +0530

    changes made to NoteItem.js

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -7,16 +7,16 @@ const NoteItem = (props) => {
     const {deleteNote}= context;
     return (
         <>
-            <div className='col-md-3'>
-                <div className="card my-3" style={{width:"15rem"}}>
+            <div className='col-md-4'>
+                <div className="card my-3" style={{width:"16rem"}}>
                     <div className="card-body">
                         <div className="d-flex align-items-center">
                             <h5 className="card-title">{note.title}</h5>
                             <i className="fa-solid fa-trash-can mx-3" onClick={()=>deleteNote(note._id,showAlert)}></i>
                             <i className="fa-solid fa-pen-to-square mx-3" onClick={()=>updateNote(note,showAlert)}></i>
                         </div>
-                        <p className="card-text">{note.description}</p>
-                        <p className="card-text">{note.tag}</p>
+                        <p className="card-text" style={{color:'white',backgroundColor:'black',borderRadius:'10px',padding:'10px'}}>{note.description}</p>
+                        <p className="card-text" style={{background:'#8282966b',color:'black',padding:'10px', borderRadius:'10px',textAlign:'center'}}>{note.tag}</p>
                         
                     </div>
                 </div>
